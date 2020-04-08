6979236a9f11f111fb4f93b4456629c75e2510dc
renaman <n.renaman@icloud.com>
2020-04-08 20:48:35 +0900

    [Fix]ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'new' => 'books#new'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
+  get 'books/:id' => 'books#show',as: 'show_books'
   get 'books/:id/edit' => 'books#edit',as: 'edit_books'
   patch 'books/:id' => 'books#update',as: 'update_books'
   delete 'books/:id' => 'books#destroy',as: 'destroy_books'
