435d98a927e14456411a3fb03b83e960264cfe48
renaman <n.renaman@icloud.com>
2020-04-09 22:32:12 +0900

    [Fix]RSpecでテスト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,18 @@
 class BooksController < ApplicationController
 	def create
 		@book = Book.new(book_params)
-		@book.save
-		redirect_to books_path(@book.id)
+		if @book.save
+			flash[:notice] = "Book was successfully created."
+		    redirect_to book_path(@book.id)
+		else
+			@books = Book.all
+			render action: :index
+		end
 	end
 	def index
 		@books = Book.all
 		@book = Book.new
+		@books = Book.all.order(created_at: :desc)
 	end
 	def show
 		@book = Book.find(params[:id])
@@ -19,10 +25,15 @@ class BooksController < ApplicationController
 		book.update(book_params)
 		redirect_to book_path(book.id)
 	end
+
 	def destroy
 		book = Book.find(params[:id])
-		book.destroy
-		redirect_to books_path
+		if book.destroy
+		   flash[:notice] = "Book was successfully destroyed."
+		   redirect_to books_path
+		else
+			render action: :new
+		end
 	end
 
 	private
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+	validates :body, presence: true
+	validates :title, presence: true
+	validates :title, :body, presence: true, on: :update
 end
